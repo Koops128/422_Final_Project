8d79f86b43adc2036b8afd41d781b7c0f87ef3fe
Abigail Smith <abilynsmith@yahoo.com>
2016-02-28 15:51:15 -0800

    Commented out
    If we keep my modifications to Pcb.h and .c, we won't need these files.

--- ProducerConsumer.h
+++ ProducerConsumer.h
@@ -1,38 +1,38 @@
-/*
- * ProducerConsumer.h
- *
- * Programming Team:
- * Sean Markus
- * Wing-Sea Poon
- * Abigail Smith
- * Tabi Stein
- *
- */
+// /*
+//  * ProducerConsumer.h
+//  *
+//  * Programming Team:
+//  * Sean Markus
+//  * Wing-Sea Poon
+//  * Abigail Smith
+//  * Tabi Stein
+//  *
+//  */
 
-#ifndef PRODUCERCONSUMER_H_
-#define PRODUCERCONSUMER_H_
+// #ifndef PRODUCERCONSUMER_H_
+// #define PRODUCERCONSUMER_H_
 
-#include "Pcb.h"
+// #include "Pcb.h"
 
-#define PRO_LOCK_UNLOCK 1
-#define CON_LOCK_UNLOCK 1
-#define PRO_WAIT 1
-#define CON_WAIT 1
-#define PRO_SIGNAL 1
-#define CON_SIGNAL 1
-#define MAX_SHARED_SIZE 5
+// #define PRO_LOCK_UNLOCK 1
+// #define CON_LOCK_UNLOCK 1
+// #define PRO_WAIT 1
+// #define CON_WAIT 1
+// #define PRO_SIGNAL 1
+// #define CON_SIGNAL 1
+// #define MAX_SHARED_SIZE 5
 
-typedef struct ProducerConsumer* ProConPtr;
+// typedef struct ProducerConsumer* ProConPtr;
 
-ProConPtr ProducerConsumerConstructor(PcbPtr prod, PcbPtr cons,
-		unsigned int proLock[], unsigned int conLock[],
-		unsigned int proUnlock[], unsigned int conUnlock[],
-		unsigned int proWait[], unsigned int conWait[],
-		unsigned int proSig[], unsigned int conSig[]);
+// ProConPtr ProducerConsumerConstructor(PcbPtr prod, PcbPtr cons,
+// 		unsigned int proLock[], unsigned int conLock[],
+// 		unsigned int proUnlock[], unsigned int conUnlock[],
+// 		unsigned int proWait[], unsigned int conWait[],
+// 		unsigned int proSig[], unsigned int conSig[]);
 
-int Wait(ProConPtr procon, PcbPtr waiter);
+// int Wait(ProConPtr procon, PcbPtr waiter);
 
-PcbPtr Signal(ProConPtr procon, PcbPtr signaler);
+// PcbPtr Signal(ProConPtr procon, PcbPtr signaler);
 
 
-#endif /* PRODUCERCONSUMER_H_ */
+// #endif /* PRODUCERCONSUMER_H_ */
