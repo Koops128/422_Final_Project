1dbf548c871c73209430b5e26f01f16e309d3794
Abigail Smith <abilynsmith@yahoo.com>
2016-03-09 11:20:06 -0800

    Adding in Tabi's stuff
    This is sort of in progress, I was in the middle of adding condition variable stuff, and then I got sidetracked with fixes some stuff with my trap handlers and how CPU calls them and what happens with context switches.

--- CPU.c
+++ CPU.c
@@ -36,19 +36,27 @@
 #define PRIORITY_LEVELS 16
 #define ROUNDS_TO_PRINT 4 // the number of rounds to wait before printing simulation data
 #define SIMULATION_END 100000 //the number of instructions to execute before the simulation may end
-#define DEADLOCK_SIMULATION 0	//1 = deadlock, 0 = no deadlock
+#define DEADLOCK	0//1			//Whether to do deadlock. 0 - no. 1 - yes.
+#define CHECK_DEADLOCK_FREQUENCY 10 //Every number of instructions we run deadlock check
+#define BLOCKED_BY_LOCK 	7
+#define LOCK_UNBLOCK
+#define NUM_MUT_REC_PAIRS 1//5
+#define NUM_MUTEXES		  NUM_MUT_REC_PAIRS * 2 //each pair has two mutexes
 
 //Global variables
 int currPID; //The number of processes created so far. The latest process has this as its ID.
 int timerCount;
+int simCounter;
 unsigned int sysStackPC;
+unsigned int currQuantum;
 FifoQueue* newProcesses;
-FifoQueue* readyProcesses;
+PQPtr readyProcesses;
 FifoQueue* terminatedProcesses;
 PcbPtr currProcess;
 Device* device1;
 Device* device2;
 MutexPtr mutexes[sizeof(MutexStr) * (PC_PROCS + MR_PROCS * 2)];
+CondVarPtr condVars[sizeof(CondVarStr) * PC_PROCS * 2];
 
 typedef enum {
 	lockTrap=0,
@@ -58,41 +66,10 @@ typedef enum {
 	noTrap=4
 } ProdConsTrapType;
 
-////MUTEX STUFF
-//typedef struct Mutex{
-//	PcbPtr owner;
-//	FifoQueue * waitQ;
-//}MutexStr;
-//
-//MutexStr* mutexes;// = (MutexStr*) malloc(sizeof(MutexStr) * (PC_PROCS + MR_PROCS * 2));
-//
-//MutexStr* MutexConstructor() {
-//	MutexStr* mutex = (MutexStr*) malloc(sizeof(MutexStr));
-//	mutex->waitQ = fifoQueueConstructor();
-//
-//	return mutex;
-//}
-//
-//void MutexDestructor(MutexStr* mutex) {
-//	fifoQueueDestructor(&mutex->waitQ);
-//	PCBDestructor(mutex->owner);
-//
-//	free(mutex);
-//	mutex = NULL;	//locally
-//}
-
-////Returns 1 or 0, 1 if the process got a hold of the lock, 0 if not
-//int MutexLock(MutexStr* mutex, PcbPtr pcb) {
-//	//TODO
-//}
-//
-//void MutexUnlock(MutexStr* mutex, PcbPtr pcb) {
-//	//TODO
-//}
 
 /*Prepares the waiting process to be executed.*/
 void dispatcher() {
-	currProcess = fifoQueueDequeue(readyProcesses);
+	currProcess = pqDequeue(readyProcesses);
 	if (currProcess) {
 		PCBSetState(currProcess, running);
 		sysStackPC = PCBGetPC(currProcess);
@@ -101,6 +78,59 @@ void dispatcher() {
 	} else {
 		printf("Ready queue is empty, no process dispatched\r\n\r\n");
 	}
+}
+
+/*=================================================
+ *				Starvation Detection
+ *=================================================*/
+/*	A process is "starving" if it hasn't run for (numProcessesInQueue)*(priorityLevel)
+ *  quantums. Appropriately, more processes in the system means each process is expected
+ *  to get less time to run. Also, lower priority level means we expect the process not
+ *  to have run for a longer time.
+ *  A "baseline" would be equal running time for each process, so we would expect a process
+ *  not to have run for (numProcessesInQueue) quantums before promoting it. Multiplying by
+ *  the priority level is thus a way to weight this so lower priority processes aren't
+ *  expected to get as much CPU time as higher priority ones.
+ */
+void runStarvationDetector() {
+	//sum up number of processes
+	int i, numProcs = 0;
+	printf("\n---Running S Detector---\n");
+	for (i = 0; i < PRIORITY_LEVELS; i++) {
+		numProcs += ((readyProcesses->priorityArray)[i])->size;
+		printf("Queue %d has %d processes\n", i, ((readyProcesses->priorityArray)[i])->size);
+	}
+
+	//Check head of each priority level (besides top) and see if it needs boosting
+	//Check head of each priority level (except last) and see if it needs to go back to original level.
+	//(Only check heads since going through the entire queues would be a lot of overhead)
+	for (i = 0; i < PRIORITY_LEVELS; i++) {
+		FifoQueue* fq = (readyProcesses->priorityArray)[i];
+		if (fq) {
+			PcbPtr pcb = fifoQueuePeek(fq);
+			if (pcb) {
+				//test if should be demoted
+				if (i < (PRIORITY_LEVELS -1) && PCBGetStarveBoostFlag(pcb)) {
+					//toggle flag and demote to lower level.
+					PCBSetPriority(pcb, i + 1);
+					PCBSetStarveBoostFlag(pcb, 0);
+				}
+				//	Test if should be promoted
+				if (i > 0 && !PCBGetStarveBoostFlag(pcb)) {
+					int threshold = numProcs * (i+1);
+					int cyclesSinceRan = currQuantum - PCBGetLastQuantum(pcb);
+					if (cyclesSinceRan > threshold ) {
+						PCBSetPriority(pcb, i - 1); //lower priority number = higher priority
+						PCBSetStarveBoostFlag(pcb, 1);
+						pqEnqueue(readyProcesses, fifoQueueDequeue(fq));
+
+						printf("Starvation detected. After not running for %d cycles, with %d processes in readyqueue, PID %d priority went from %d to %d.\n", cyclesSinceRan, numProcs, PCBGetID(pcb), i, i-1);
+					}
+				}
+			}
+		}
+	}
+	printf("---Exiting S Detector---\n");
 }
 
 //Scheduler
@@ -148,6 +178,12 @@ void scheduler(int interruptType) {
 			dispatcher();
 		}
 		break;
+	case BLOCKED_BY_LOCK :
+		if (currProcess) {
+			PCBSetPC(currProcess, sysStackPC); //save it's pc, but don't put in ready queue; the mutex wait queue is holding it.
+			dispatcher();
+		}
+		break;
 	default :
 		break;
 	}
@@ -184,7 +220,7 @@ int setIOTimer(Device* device) {
  * The interrupt service routine for a IO interrupt
  * Does it still have to save Cpu state to Pcb? */
 void IO_ISR(int numIO) {	//IOCompletionHandler
-	if (currProcess/*PCBGetState(currProcess) != blocked && PCBGetState(currProcess) != terminated*/) {
+	if (currProcess) {
 		saveCpuToPcb();
 		PCBSetState(currProcess, interrupted);
 	}
@@ -227,25 +263,27 @@ void IOTrapHandler(Device* d) {
 		setIOTimer(d);
 	}
 	scheduler(IO_COMPLETION);
-	//dispatcher();
 }
 
-void ProdConsTrapHandler(ProdConsTrapType trapRequest) {
+/*returns 0 if no context switch, 1 if context switch*/
+int ProdConsTrapHandler(ProdConsTrapType trapRequest) {
 	saveCpuToPcb();
 	PCBSetState(currProcess, blocked);
 	PcbPtr returned;
+	int contextSwitch = 0; //0 for no context switch, 1 for context switch
 
 	switch(trapRequest) {
 		case lockTrap :
 			//if lock, then call the pair's mutex for a lock.  If it needs to wait for the lock,
 			//then call the scheduler with an interrupt
-			if(!MutexLock(mutexes[PCBGetPRData(currProcess)->mutex], currProcess)) {
+			if(!MutexLock(mutexes[PCBGetPCData(currProcess)->mutex], currProcess)) {
 				scheduler(PRO_CON_INTERRUPT);
+				contextSwitch = 1;
 			}
 			break;
 		case unlockTrap :
 			//if unlock, then call the pair's mutex to unlock
-			MutexUnlock(mutexes[PCBGetPRData(currProcess)->mutex], currProcess);
+			MutexUnlock(mutexes[PCBGetPCData(currProcess)->mutex], currProcess);
 			PCBSetState(currProcess, ready);
 			break;
 		case signalTrap :
@@ -328,6 +366,117 @@ int timerCheck() {
 	} else {
 		timerCount = TIMER_QUANTUM;
 		return 1;
+	}
+}
+
+/*=================================================
+ *				Deadlock Check
+ *=================================================*/
+
+/**
+ * Checks if the pcb is locked by another pcb
+ * and returns the Mutex owner pcb if it is, null otherwise
+ */
+PcbPtr isLocked(PcbPtr owner) {
+	int i, j;
+	for (i = 0; i < NUM_MUTEXES; i++) {
+		MutexPtr m = mutexes[i];
+		if (m->owner != NULL && MutexHasWaiting(m)) {
+			for (j = 0; j < m->waitQ->size; j++) {
+				if (fifoQueueContains(m->waitQ, owner) != -1) { //being locked, return mutex owner
+					return m->owner;
+				}
+			}
+		}
+	}
+	return NULL;
+}
+
+/*
+ * Checks the chain of pcbs being locked by this pcb
+ *
+ * PcbPtr owner the pcb being checked
+ * Returns 1 if pcb is deadlocked, 0 otherwise
+ */
+int checkLock(PcbPtr owner) {
+	PcbPtr parent = isLocked(owner);
+	while (parent != NULL) {//check what its locked by repeatedly
+		if (owner == parent) { //locked by lock itself is locking
+			return 1;
+		}
+		parent = isLocked(parent);//else check what that pcb is locked by
+	}
+	return 0; //pcb is not locked, chain is done
+}
+
+/**
+ * Returns 1 if true 0 otherwise
+ */
+int deadlockDetect() {
+	int i, r;
+	for (i = 0; i < NUM_MUTEXES; i++) {
+		if (mutexes[i]->owner != NULL) {
+			r = checkLock(mutexes[i]->owner);
+			if (r == 1) {
+				printf("\r\nDeadlock detected for process %d", PCBGetID(mutexes[i]->owner));
+				return 1;
+			}
+		}
+	}
+	printf("\r\nno deadlock detected\r\n");
+	return 0;
+}
+
+/*=================================================
+ *			Mutex Lock/Unlock Check
+ *=================================================*/
+
+/*Returns 1 if the current pcb a) got the lock or b) didn't want a lock. In this case,
+ *normal execution can continue.
+ *Returns 0 if the current pcb is blocked waiting for the lock. In this case, another process
+ *must take over execution.*/
+int notBlockedByLock() {
+	int notBlocked = 0;
+	MutexPtr selected = NULL;
+	if (isMutexLockStep(currProcess, 1, sysStackPC)) {
+		int index = PCBGetMutexIndex(currProcess, 1) % (NUM_MUTEXES); //Must put in parens since NUM_MUTEXES is an expression, not a value.
+		selected = mutexes[index];
+	} else if (isMutexLockStep(currProcess, 2, sysStackPC)) {
+		int index = PCBGetMutexIndex(currProcess, 2) % (NUM_MUTEXES);
+		selected = mutexes[index];
+	}
+	if (selected) {
+		notBlocked = MutexLock(selected, currProcess); //Wants lock; result depends on whether it was blocked.
+	} else {
+		notBlocked = 1; //Didn't want lock
+	}
+	return notBlocked;
+}
+
+/*If we're on an unlock step for the current pcb, then unlocks that mutex.
+ *If there was a pcb waiting for that mutex, then that pcb is returned;
+ *otherwise, null is returned.
+ *return: PcbPtr -- the PCB that was waiting for the lock; else, null.*/
+PcbPtr checkUnlock() {
+	PcbPtr unlockedPcb = NULL;
+	MutexPtr selected = NULL;
+	if (isMutexUnlockStep(currProcess, 1, sysStackPC)) {
+		selected = mutexes[PCBGetMutexIndex(currProcess, 1) % (NUM_MUTEXES)];
+	} else if (isMutexUnlockStep(currProcess, 2, sysStackPC)) {
+		selected = mutexes[PCBGetMutexIndex(currProcess, 2) % (NUM_MUTEXES)];
+	}
+	if (selected) {
+		MutexUnlock(selected, currProcess);
+		unlockedPcb = selected->owner; //NULL if none had been waiting
+	}
+	return unlockedPcb;
+}
+
+void printIfInCriticalSection() {
+	MutexPtr Mutex1 = mutexes[PCBGetMutexIndex(currProcess, 1)];
+	MutexPtr Mutex2 = mutexes[PCBGetMutexIndex(currProcess, 2)];
+	if (Mutex1->owner == currProcess && Mutex2->owner == currProcess) {
+		printf("~~PID %d is in critical section~~\n", PCBGetID(currProcess));
 	}
 }
 
@@ -435,17 +584,23 @@ int checkTermCountAndTermination() {
 	return 0;
 }
 
-void checkIOTraps() {
+/*returns 0 if no trap request, 1 if yes*/
+int checkIOTraps() {
 	if (currProcess && checkIORequest(1) != 0) {
 		printf("I/O trap request: I/O device 1, ");
 		IOTrapHandler(device1);
+		return 1;
 	}
 
 	if (currProcess && checkIORequest(2) != 0) {
 		printf("I/O trap request: I/O device 2, ");
 		IOTrapHandler(device2);
+		return 1;
 	}
+	return 0;
 }
+
+
 
 void checkPCTraps() {
 	ProdConsTrapType trapRequest = checkProdConsRequest();
@@ -455,7 +610,20 @@ void checkPCTraps() {
 }
 
 void checkMRTraps() {
+	if (!notBlockedByLock()) {
+		//TODO make an isr for this
+		scheduler(BLOCKED_BY_LOCK);
+		simCounter++;
+		continue;
+	} else {
+		PcbPtr wasWaitingPcb = checkUnlock();
+		if (wasWaitingPcb) {
+			//TODO make an isr for this
+			pqEnqueue(readyProcesses, wasWaitingPcb);
+		}
+	}
 
+	printIfInCriticalSection();
 }
 
 void cpu() {
@@ -465,7 +633,7 @@ void cpu() {
 
 		printf("\r\nBegin Simulation:\r\n\r\n");
 
-		int simCounter = 0;
+		simCounter = 0;
 
 		while (simCounter <= SIMULATION_END) {
 
@@ -483,7 +651,9 @@ void cpu() {
 			sysStackPC++;
 
 /****Checking traps****/
-			checkIOTraps();
+			if (checkIOTraps()) {
+				continue;
+			}
 			RelationshipPtr relationship = PCBGetRelationship(currProcess);
 			if (relationship->mType == producer || relationship->mType == consumer) {
 				checkPCTraps();
@@ -508,7 +678,7 @@ int main(void) {
 	sysStackPC = 0;
 	timerCount = TIMER_QUANTUM;
 	newProcesses = fifoQueueConstructor();
-	readyProcesses = fifoQueueConstructor();
+	readyProcesses = pqConstructor();
 	terminatedProcesses = fifoQueueConstructor();
 	device1 = DeviceConstructor();
 	device2 = DeviceConstructor();
@@ -530,7 +700,7 @@ int main(void) {
 
 	//free all the things!
 	fifoQueueDestructor(&newProcesses);
-	fifoQueueDestructor(&readyProcesses);
+	pqDestructor(readyProcesses);
 	fifoQueueDestructor(&terminatedProcesses);
 
 	DeviceDestructor(device1);
